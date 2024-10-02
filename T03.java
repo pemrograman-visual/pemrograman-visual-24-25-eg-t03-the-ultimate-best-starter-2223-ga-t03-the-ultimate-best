73c7c44b446747be975a6f48b22474923f3f30d8
LucasPardede <lucaspardede184@gmail.com>
2024-10-02 16:44:30 +0700

    PAIR21

--- T03.java
+++ T03.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T03 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
